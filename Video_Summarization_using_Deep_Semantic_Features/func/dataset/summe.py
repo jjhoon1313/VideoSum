1d1b3d0642104098a1a4a787cd575244f6f5c3fd
Jonghun Jeong <jjhoon1313@gmail.com>
2018-04-17 12:44:52 +0900

    second commit

---
+++ Video_Summarization_using_Deep_Semantic_Features/func/dataset/summe.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import json
+import numpy as np
+
+
+datasetRoot = 'data/summe/'
+
+
+class SUMME():
+
+    def __init__(self, video_id, feat_type='vgg'):
+
+        dataset = json.load(open(datasetRoot + 'dataset.json'))
+        print ('load ' + video_id)
+        data = list(filter(lambda x: x['videoID'] == video_id, dataset))
+        self.data = data[0]
+        self.feat = np.load(datasetRoot + 'feat/' + feat_type +
+                            '/' + video_id + '.npy').astype(np.float32)
+
+    def sampleFrame(self):
+        fps = self.data['fps']
+        fnum = self.data['fnum']
+
+        idx = np.arange(fps, fnum, fps)
+        idx = np.floor(idx)
+        idx = idx.tolist()
+        idx = map(int, idx)
+
+        img = [self.data['image'][i] for i in idx]
+        img_id = [self.data['imgID'][i] for i in idx]
+        score = [self.data['score'][i] for i in idx]
+
+        return img, img_id, score
